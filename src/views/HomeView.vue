5b0ade0472b0ef661c949db8663b43257d9edbd6
Davi Amaral <davisa0876@gmail.com>
2023-08-05 10:56:14 -0400

    add increaseCounter and decreaseCounter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="home">
-    <button class="btn btn-primary shadow">-</button>
-    <span class="counter"> 0 </span>
-    <button class="btn btn-primary shadow">+</button>
+    <button @click="decreaseCounter" class="btn btn-primary shadow">-</button>
+    <span class="counter"> {{counter}} </span>
+    <button @click="increaseCounter" class="btn btn-primary shadow">+</button>
   </div>
 </template>
+<script>
+export default{
+  data()  {
+      return{
+        counter: 0
+      }
+    },
+    methods: {
+      increaseCounter() {
+        this.counter++
+      },
+      decreaseCounter() {
+        this.counter--
+      }
+    }
+}
+</script>
 <style>
  .home{
   text-align: center;
-
  }
  .btn, .counter{
   font-size: 40px;
